b86c81239e1c32a51345c1c468c03fa00d827b3e
TejashwarRao <tejashwarrao18@gmail.com>
2026-05-08 16:50:51 +0530

    Implemented JWT authentication system

--- auth-service/auth-service/src/main/java/com/rapido/auth_service/config/SecurityConfig.java
+++ auth-service/auth-service/src/main/java/com/rapido/auth_service/config/SecurityConfig.java
@@ -2,20 +2,45 @@ package com.rapido.auth_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) throws Exception {
+
         http
+
+                // Disable CSRF
                 .csrf(csrf -> csrf.disable())
+
+                // Allow public APIs
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/health").permitAll()
+
+                        .requestMatchers(
+                                "/auth/register",
+                                "/auth/login",
+                                "/health"
+                        ).permitAll()
+
+                        // Other APIs secured
                         .anyRequest().authenticated()
-                );
+                )
+
+                // Enable basic auth
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
---
+++ auth-service/auth-service/src/main/java/com/rapido/auth_service/security/JwtFilter.java
@@ -0,0 +1,73 @@
+package com.rapido.auth_service.security;
+
+import com.rapido.auth_service.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        final String authHeader =
+                request.getHeader("Authorization");
+
+        String jwt = null;
+        String email = null;
+
+        if (authHeader != null &&
+                authHeader.startsWith("Bearer ")) {
+
+            jwt = authHeader.substring(7);
+
+            email = jwtUtil.extractEmail(jwt);
+        }
+
+        if (email != null &&
+                SecurityContextHolder.getContext()
+                        .getAuthentication() == null) {
+
+            if (jwtUtil.validateToken(jwt)) {
+
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                email,
+                                null,
+                                Collections.emptyList()
+                        );
+
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource()
+                                .buildDetails(request)
+                );
+
+                SecurityContextHolder
+                        .getContext()
+                        .setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
